141c02cecedd19941e0ffae71ea468d91807533f
Williams Leon <wdleonm@gmail.com>
2026-05-24 17:28:04 -0400

    chore: ignorar errores de eslint y typescript en la build de produccion

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Ignorar errores de ESLint durante la compilación de producción.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Ignorar errores de TypeScript durante la compilación de producción.
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
